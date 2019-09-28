37728fce0ab8ee54edd491f8351f4cfdb894bf24
Fonki <andre.fonk@gmx.de>
2019-09-28 17:34:55 +0200

    changes

--- app/src/main/java/com/example/plankr/Main.java
+++ app/src/main/java/com/example/plankr/Main.java
@@ -1,7 +1,8 @@
 package com.example.plankr;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
@@ -10,6 +11,14 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.Username);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 
+            }
+        });
+
 
         
     }
