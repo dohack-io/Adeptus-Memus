0e5892e42e6b12e4abddb55837917f5a0d355f6e
faxi1 <fabian.sich001@stud.fh-dortmund.de>
2019-09-29 01:15:26 +0200

    Database

--- app/src/main/java/com/example/plankr/Main.java
+++ app/src/main/java/com/example/plankr/Main.java
@@ -1,7 +1,9 @@
 package com.example.plankr;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
@@ -9,6 +11,17 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
+        /*Button btn = findViewById(R.id.Username);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Intent intent = new Intent(Main.this, Profil.class);
+                  startActivity(intent);
+            }
+        });*/
+
+
+        
     }
 }
