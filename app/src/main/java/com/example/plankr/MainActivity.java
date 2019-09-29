4cbdf5aa7adbee00a3b228d736057d1b7aaec590
faxi1 <fabian.sich001@stud.fh-dortmund.de>
2019-09-29 03:58:28 +0200

    LoginButton

--- app/src/main/java/com/example/plankr/MainActivity.java
+++ app/src/main/java/com/example/plankr/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.plankr;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private Button Register;
     private int counter = 5;
     private Button userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         Login = (Button)findViewById(R.id.register_sign_in);
         userRegistration = findViewById(R.id.register_ready);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
 
         userRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,12 +54,51 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-       /* Login.setOnClickListener(new View.OnClickListener() {
+       Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                 validate(Name.getText().toString(), Password.getText().toString());
+
             }
-        });*/
+        });
+
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, Main.class));
+        }
+    }
+
+    private void validate(String name, String password){
+
+        progressDialog.setMessage("Wait, until the alcohol kicks in!");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(name, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, Main.class));
+                } else  {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    counter--;
+                    progressDialog.dismiss();
+
+                    if(counter != 0){
+                        Toast.makeText(MainActivity.this, "Login-Attempts: " + counter, Toast.LENGTH_LONG).show();
+                    } else {
+                        Login.setEnabled(false);
+                        Toast.makeText(MainActivity.this, "Login-Button disabled" + counter, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 
 }
